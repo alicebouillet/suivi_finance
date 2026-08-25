416ec0b238f88dcd995df1af48fb7cbe5245c72c
alicebouillet <117653992+alicebouillet@users.noreply.github.com>
2026-08-25 16:18:24 +0200

    Update Supabase URL and anon key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Remplis ces deux valeurs avec celles de ton projet Supabase
 // (Project Settings > API dans le dashboard Supabase)
-const SUPABASE_URL = "https://TON-PROJET.supabase.co";
-const SUPABASE_ANON_KEY = "TA_CLE_ANON_PUBLIC";
+const SUPABASE_URL = "https://gctxitppsoecfrynaabi.supabase.co";
+const SUPABASE_ANON_KEY ="sb_publishable_Sm7zSpF6csROPVmmAEAXMw_Ycgl_7y9";
